aba1e804fd7fb71c6fc79c300514de0887997c4d
Dawik Dawikk <blodawiq@gmail.com>
2024-09-11 15:10:44 +0200

    Deleted unnecessary data

--- android/build/intermediates/cxx/Debug/304g3u4q/logs/x86/build_command_stockfish.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\Dawid\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\_Mobile\\ChessEngine\\node_modules\\react-native-stockfish-android\\android\\.cxx\\Debug\\304g3u4q\\x86" ^
-  stockfish
